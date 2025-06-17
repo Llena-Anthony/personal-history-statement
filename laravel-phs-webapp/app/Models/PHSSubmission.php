eab390901c04e77b66af35349cf16f28e33f4781
JOHN HENRICH COLLO <2235110@slu.edu.ph>
2025-06-17 09:27:10 +0800

    Fix admin dashboard, resolve model/table issues, cleanup old files

---
+++ laravel-phs-webapp/app/Models/PHSSubmission.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PHSSubmission extends Model
+{
+    use HasFactory;
+
+    protected $table = 'phs_submissions';
+    
+    protected $fillable = [
+        'user_id',
+        'status',
+        'admin_notes',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function personalInfo()
+    {
+        return $this->hasOne(PersonalInfo::class, 'username', 'user_id');
+    }
+
+    public function familyHistory()
+    {
+        return $this->hasMany(FamilyHistory::class, 'username', 'user_id');
+    }
+
+    public function educationalBackground()
+    {
+        return $this->hasOne(EducationalBackground::class, 'username', 'user_id');
+    }
+
+    public function employmentHistory()
+    {
+        return $this->hasMany(EmploymentHistory::class, 'username', 'user_id');
+    }
+
+    public function militaryHistory()
+    {
+        return $this->hasOne(MilitaryHistory::class, 'username', 'user_id');
+    }
+} 
